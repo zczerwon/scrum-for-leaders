1ab9755e11bde291199706fc5ca2d91f62fd7b50
Zbigniew Czerwonka <zczerwonka@gmail.com>
2024-01-06 20:48:25 +0100

    First version of Scrum For Leaders ReactJS web app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const avatarUrl = "https://avatars.githubusercontent.com/u/18039995?v=4";
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={avatarUrl} alt="Zbigniew Czerwonka portreit"/>
+                <p>Welcome on Scrum For Leaders!</p>
+                <a className="App-link" href="https://scrum.org" target="_blank" rel="noreferrer">
+                    For more learning go to scrum.org
+                </a>
+                <a className="github-link" href="https://github.com/zczerwon">
+                    <img alt="github logo" src={"github-logo.png"}/>
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
